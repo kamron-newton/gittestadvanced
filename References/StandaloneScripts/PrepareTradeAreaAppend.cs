f7db406e4434462f625303db0d449c9c5f219e7b
phimagru <>
2021-10-26 13:25:56 -0500

    Implement standalone script loading framework #565172

---
+++ References/StandaloneScripts/PrepareTradeAreaAppend.cs
@@ -0,0 +1,4 @@
+var sql = "UPDATE T_RETAIL_MATCHBACK_QUEUE SET CURRENT_STATUS = 'R', CURRENT_STEP_NAME = 'External' WHERE ANON_TRANSACTION_ID = :transactionId";
+var parameters = new DbParameterCollection(); 
+parameters.Add(new DbParameter("transactionId", new Guid(input.TransactionId.ToString()), DbType.Guid));
+verb.ModifyDatabase(sql, parameters);

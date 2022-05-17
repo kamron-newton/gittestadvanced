0b73d5ffa95266c2d0f06a1faae18d5c9d543223
anisekha <anisekha@publicisgroupe.net>
2022-05-17 11:10:06 +0530

    updated forge scripts

---
+++ ForgeScripts/AutomationExecutionAws_A.sh
@@ -0,0 +1,7 @@
+#Run Basic tag
+sudo /usr/local/bin/testengine -u "admin" -p "Admin@123" -H "http://localhost:8080" run project tags Basic environment=#{TenantCode}#  reportFileName Basic output=/home/ec2-user/testEngine/reports format pdf printReport /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+aws s3 cp /home/ec2-user/testEngine/reports/Basic.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Basic.pdf
+
+#Run CrimsonHawks tag
+sudo /usr/local/bin/testengine -u "admin" -p "Admin@123" -H "http://localhost:8080" run project tags CrimsonHawks environment=#{TenantCode}#  reportFileName CrimsonHawks output=/home/ec2-user/testEngine/reports format pdf printReport /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+aws s3 cp /home/ec2-user/testEngine/reports/CrimsonHawks.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/CrimsonHawks.pdf

616a39673164891ff8149f3e80720e0332680cba
anisekha <anisekha@publicisgroupe.net>
2022-07-07 14:37:51 +0530

    finished ReturnTransactionMomentCap test

--- ForgeScripts/AutomationExecutionAws_A.sh
+++ ForgeScripts/AutomationExecutionAws_A.sh
@@ -1,34 +1,34 @@
 #Run Basic tag and upload results to S3
 echo "starting script A `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Basic environment=#{TenantCode}# reportFileName Basic output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
-echo "Finished queueing Basic test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
+echo "Finished running Basic test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws s3 cp /home/ec2-user/testEngine/reports/Basic.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Basic.pdf
 
 #Run CrimsonHawks and upload results to S3
 echo "Queueing CH test `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags CrimsonHawks environment=#{TenantCode}# reportFileName CrimsonHawks output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
-echo "Finished queueing CH test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
+echo "Finished running CH test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws s3 cp /home/ec2-user/testEngine/reports/CrimsonHawks.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/CrimsonHawks.pdf
 
 #Run Newton tag and upload results to S3
 echo "Queueing Newton test `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Newton environment=#{TenantCode}# reportFileName Newton output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
-echo "Finished queueing Newton test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
+echo "Finished running Newton test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws s3 cp /home/ec2-user/testEngine/reports/Newton.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Newton.pdf
 
 #Run Synergy tag and upload results to S3
 echo "Queueing Synergy test `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Synergy environment=#{TenantCode}# reportFileName Synergy output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
-echo "Finished queueing Synergy test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
+echo "Finished running Synergy test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws s3 cp /home/ec2-user/testEngine/reports/Synergy.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Synergy.pdf
 
 
 #shutdown the license server instance
 echo "Shutting down license server `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws ec2 stop-instances --region "us-east-1" --instance-ids "#{LicenseServerInstanceId}#"
-echo "Shutdown command complete, result of it is `$?` `date +%T`" >> /home/ec2-user/testEngine/log.txt
 
 #wait for last upload to finish
+echo "Sleeping for 5 minutes to wait for upload to S3 to finish `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sleep 300
 echo "Finished sleeping for 5 mins, shutting down instance `date +%T`" >> /home/ec2-user/testEngine/log.txt
 
--- ForgeScripts/AutomationExecutionAws_B.sh
+++ ForgeScripts/AutomationExecutionAws_B.sh
@@ -1,20 +1,29 @@
-#Run Basic tag nad upload results to S3
+#Run Basic tag and upload results to S3
+echo "starting script B `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Basic environment=#{TenantCode}# reportFileName Basic output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+echo "Finished running Basic test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws s3 cp /home/ec2-user/testEngine/reports/Basic.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Basic.pdf
 
-#Run People tag nad upload results to S3
+#Run People tag and upload results to S3
+echo "Queueing People test `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags People environment=#{TenantCode}# reportFileName People output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+echo "Finished running People test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws s3 cp /home/ec2-user/testEngine/reports/People.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/People.pdf
 
-#Run Delta tag nad upload results to S3
+#Run Delta tag and upload results to S3
+echo "Queueing Delta test `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Delta environment=#{TenantCode}# reportFileName Delta output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+echo "Finished running Delta test, uploading report to S3 `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws s3 cp /home/ec2-user/testEngine/reports/Delta.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Delta.pdf
 
 #shutdown the license server instance
+echo "Shutting down license server `date +%T`" >> /home/ec2-user/testEngine/log.txt
 aws ec2 stop-instances --region "us-east-1" --instance-ids "#{LicenseServerInstanceId}#"
 
 #wait for last upload to finish
+echo "Sleeping for 5 minutes to wait for upload to S3 to finish `date +%T`" >> /home/ec2-user/testEngine/log.txt
 sleep 300
+echo "Finished sleeping for 5 mins, shutting down instance `date +%T`" >> /home/ec2-user/testEngine/log.txt
 
 #shutdown instance
 sudo shutdown now

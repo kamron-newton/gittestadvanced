14ba30dc0f885254ab68fd58a3906314d972ea4a
Deepika Vangapally <deevanga@publicisgroupe.net>
2022-08-03 07:50:44 -0500

    #690686 Update to forge scripts for email and file name

--- ForgeScripts/AutomationExecutionAws_A.sh
+++ ForgeScripts/AutomationExecutionAws_A.sh
@@ -1,30 +1,30 @@
 #Run Basic tag and upload results to S3
 echo "starting script A `date +%T`" >> /home/ec2-user/testEngine/log.txt
-sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Basic environment=#{TenantCode}# reportFileName Basic output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Basic environment=#{TenantCode}# reportFileName #{Release.ReleaseName}#"-"Basic output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
 echo "Finished running Basic test, uploading report to S3 and sending email `date +%T`" >> /home/ec2-user/testEngine/log.txt
-aws s3 cp /home/ec2-user/testEngine/reports/Basic.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Basic.pdf
-sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Basic #{TenantCode}# #{FullReleaseVersion}# #{EnvironmentType}# `date +"%m/%d/%Y"` `date +"%r"` `date +"%p"`
+aws s3 cp /home/ec2-user/testEngine/reports/#{Release.ReleaseName}#"-"Basic.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/#{Release.ReleaseName}#"-"Basic.pdf
+sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Basic #{Release.ReleaseName}#"-"Basic
 
 #Run CrimsonHawks and upload results to S3
 echo "Queueing CH test `date +%T`" >> /home/ec2-user/testEngine/log.txt
-sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags CrimsonHawks environment=#{TenantCode}# reportFileName CrimsonHawks output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags CrimsonHawks environment=#{TenantCode}# reportFileName #{Release.ReleaseName}#"-"CrimsonHawks output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
 echo "Finished running CH test, uploading report to S3 and sending email `date +%T`" >> /home/ec2-user/testEngine/log.txt
-aws s3 cp /home/ec2-user/testEngine/reports/CrimsonHawks.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/CrimsonHawks.pdf
-sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py CrimsonHawks #{TenantCode}# #{FullReleaseVersion}# #{EnvironmentType}# `date +"%m/%d/%Y"` `date +"%r"` `date +"%p"`
+aws s3 cp /home/ec2-user/testEngine/reports/#{Release.ReleaseName}#"-"CrimsonHawks.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/#{Release.ReleaseName}#"-"CrimsonHawks.pdf
+sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py CrimsonHawks #{Release.ReleaseName}#"-"CrimsonHawks
 
 #Run Newton tag and upload results to S3
 echo "Queueing Newton test `date +%T`" >> /home/ec2-user/testEngine/log.txt
-sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Newton environment=#{TenantCode}# reportFileName Newton output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Newton environment=#{TenantCode}# reportFileName #{Release.ReleaseName}#"-"Newton output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
 echo "Finished running Newton test, uploading report to S3 and sending email `date +%T`" >> /home/ec2-user/testEngine/log.txt
-aws s3 cp /home/ec2-user/testEngine/reports/Newton.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Newton.pdf
-sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Newton #{TenantCode}# #{FullReleaseVersion}# #{EnvironmentType}# `date +"%m/%d/%Y"` `date +"%r"` `date +"%p"`
+aws s3 cp /home/ec2-user/testEngine/reports/#{Release.ReleaseName}#"-"Newton.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/#{Release.ReleaseName}#"-"Newton.pdf
+sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Newton #{Release.ReleaseName}#"-"Newton
 
 #Run Synergy tag and upload results to S3
 echo "Queueing Synergy test `date +%T`" >> /home/ec2-user/testEngine/log.txt
-sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Synergy environment=#{TenantCode}# reportFileName Synergy output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Synergy environment=#{TenantCode}# reportFileName #{Release.ReleaseName}#"-"Synergy output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
 echo "Finished running Synergy test, uploading report to S3 and sending email `date +%T`" >> /home/ec2-user/testEngine/log.txt
-aws s3 cp /home/ec2-user/testEngine/reports/Synergy.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Synergy.pdf
-sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Synergy #{TenantCode}# #{FullReleaseVersion}# #{EnvironmentType}# `date +"%m/%d/%Y"` `date +"%r"` `date +"%p"`
+aws s3 cp /home/ec2-user/testEngine/reports/#{Release.ReleaseName}#"-"Synergy.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/#{Release.ReleaseName}#"-"Synergy.pdf
+sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Synergy #{Release.ReleaseName}#"-"Synergy
 
 
 #shutdown the license server instance
--- ForgeScripts/AutomationExecutionAws_B.sh
+++ ForgeScripts/AutomationExecutionAws_B.sh
@@ -1,23 +1,23 @@
 #Run Basic tag and upload results to S3
 echo "starting script B `date +%T`" >> /home/ec2-user/testEngine/log.txt
-sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Basic environment=#{TenantCode}# reportFileName Basic output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Basic environment=#{TenantCode}# reportFileName #{Release.ReleaseName}#"-"Basic output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
 echo "Finished running Basic test, uploading report to S3 and sending email `date +%T`" >> /home/ec2-user/testEngine/log.txt
-aws s3 cp /home/ec2-user/testEngine/reports/Basic.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Basic.pdf
-sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Basic #{TenantCode}# #{FullReleaseVersion}# #{EnvironmentType}# `date +"%m/%d/%Y"` `date +"%r"` `date +"%p"`
+aws s3 cp /home/ec2-user/testEngine/reports/#{Release.ReleaseName}#"-"Basic.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/#{Release.ReleaseName}#"-"Basic.pdf
+sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Basic #{Release.ReleaseName}#"-"Basic
 
 #Run People tag and upload results to S3
 echo "Queueing People test `date +%T`" >> /home/ec2-user/testEngine/log.txt
-sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags People environment=#{TenantCode}# reportFileName People output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags People environment=#{TenantCode}# reportFileName #{Release.ReleaseName}#"-"People output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
 echo "Finished running People test, uploading report to S3 and sending email `date +%T`" >> /home/ec2-user/testEngine/log.txt
-aws s3 cp /home/ec2-user/testEngine/reports/People.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/People.pdf
-sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py People #{TenantCode}# #{FullReleaseVersion}# #{EnvironmentType}# `date +"%m/%d/%Y"` `date +"%r"` `date +"%p"`
+aws s3 cp /home/ec2-user/testEngine/reports/#{Release.ReleaseName}#"-"People.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/#{Release.ReleaseName}#"-"People.pdf
+sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py People #{Release.ReleaseName}#"-"People
 
 #Run Delta tag and upload results to S3
 echo "Queueing Delta test `date +%T`" >> /home/ec2-user/testEngine/log.txt
-sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Delta environment=#{TenantCode}# reportFileName Delta output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
+sudo /usr/local/bin/testengine -u "#{TestEngineUsername}#" -p "#{TestEnginePassword}#" -H "http://localhost:8080" run project tags Delta environment=#{TenantCode}# reportFileName #{Release.ReleaseName}#"-"Delta output=/home/ec2-user/testEngine/reports format pdf /home/ec2-user/testEngine/FusionWebApiAutomationReadyv2.zip
 echo "Finished running Delta test, uploading report to S3 and sending email `date +%T`" >> /home/ec2-user/testEngine/log.txt
-aws s3 cp /home/ec2-user/testEngine/reports/Delta.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/Delta.pdf
-sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Delta #{TenantCode}# #{FullReleaseVersion}# #{EnvironmentType}# `date +"%m/%d/%Y"` `date +"%r"` `date +"%p"`
+aws s3 cp /home/ec2-user/testEngine/reports/#{Release.ReleaseName}#"-"Delta.pdf s3://#{S3BucketName}#/#{TenantCode}#/TestResultsV2/#{Release.ReleaseName}#/#{Release.ReleaseName}#"-"Delta.pdf
+sudo python3 /home/ec2-user/testEngine/codeDeploy/send_pdf_mail.py Delta #{Release.ReleaseName}#"-"Delta
 
 #shutdown the license server instance
 echo "Shutting down license server `date +%T`" >> /home/ec2-user/testEngine/log.txt

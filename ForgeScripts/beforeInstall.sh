329de03509d2c168a46b66e0ed857e9337524ee2
raks1 <raks1@publicisgroupe.net>
2022-09-16 10:09:19 +0530

    Updated forge
    Related Work Items: #713492

--- ForgeScripts/beforeInstall.sh
+++ ForgeScripts/beforeInstall.sh
@@ -1,10 +1,10 @@
 #clean out code deploy directory 
 cd /home/ec2-user/testEngine/codeDeploy
-rm -rf *
+sudo rm -rf *
 
 #clean out the reports directory
 cd /home/ec2-user/testEngine/reports
-rm -rf *
+sudo rm -rf *
 
 #clean the log file
 truncate -s 0 /home/ec2-user/testEngine/log.txt
